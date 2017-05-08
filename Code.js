fd58ff9c9af0773ee986155e0db4c24ebe3599cd
Aadam Rahat Mohammad <armohamm@be-linux02.lt.ucsc.edu>
2017-05-07 17:49:07 -0700

    Changed to werds

--- Code.js
+++ Code.js
@@ -3,7 +3,7 @@ function onLoad(){
 
 	I never asked, but why does Octocat hate Octodog?
 
-	HAHA, I DELETED THIS LINE OF CODE! NOW THERE'S NO WAY TO RESOLVE THE CONFLICT BETWEEN OCTOCAT AND OCTODOG!
+	WERDS!
 }
 
 I chaged code here just 'cause I can.
