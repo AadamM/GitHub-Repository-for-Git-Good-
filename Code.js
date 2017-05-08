dde53f433690bd7c634886bf692a82141b9133e2
Aadam Rahat Mohammad <armohamm@ucsc.edu>
2017-05-07 18:25:00 -0700

    I forgot to put a message, but I hope this will suffice.

--- Code.js
+++ Code.js
@@ -9,3 +9,5 @@ function onLoad(){
 I changed this file via GitHub.com. I don't really know how to prove that, but just take my word for it...
 
 I chaged code here just 'cause I can.
+
+I made another new branch because in the assignment description, its says it should have "at least one new branch". I'm not sure if that's in reference to the branches we made in 2iv and 2vi, so I just made this one anyway.
