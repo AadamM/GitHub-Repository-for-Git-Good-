cffa4a0cead4d7041576e449c74e2b4ccd184fa2
Aadam Rahat Mohammad <armohamm@ucsc.edu>
2017-05-07 18:19:11 -0700

    Changed NewCode.js

--- NewCode.js
+++ NewCode.js
@@ -1,1 +1,3 @@
-This is new content. I didn't bother to make it look like code because you know what they say, "Fool me once, shame on you. Fool me twice, shame on me". Therefore, there's no reason I should format this like code, I guess. 
+This is new content. I didn't bother to make it look like code because you know what they say, "Fool me once, shame on you. Fool me twice, shame on me". Therefore, there's no reason I should format this like code, I guess.
+
+Don't think I didn't forget about you, NewCode.js. <3 
